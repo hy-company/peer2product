6c09cd4ef432492729400fca67b62305e5fa4d13
David <david@dkvdev.com>
2022-02-16 17:53:08 +0100

    added font awesome to the front-end. changed some icons to font awesome icons. added a javascript function to hide the cart on click.

--- ui/js/peer2product-frontendfunctions.js
+++ ui/js/peer2product-frontendfunctions.js
@@ -1,4 +1,5 @@
 var navbarAboutDropDownState = "inactive";	//value determines behaviour of navbarAboutDropDown onclick event 
+var cartToggleState = 1;
 
 function navbarAboutDropDown(){			//onlick event
 
@@ -10,6 +11,17 @@ function navbarAboutDropDown(){			//onlick event
         document.getElementById('aboutLinks').style.display = "none";
         navbarAboutDropDownState = "inactive";
         // console.log("state: 0");
+    }
+
+}
+
+function toggleCart(){      //onclick event
+    if(cartToggleState == 1){
+        document.getElementById('sidebar').style.display = "none";
+        cartToggleState= 0;
+    }else{
+        document.getElementById('sidebar').style.display = "block";
+        cartToggleState = 1;
     }
 
 }
