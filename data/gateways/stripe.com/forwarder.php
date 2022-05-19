a3803ec82d934ef41bde766ba0bc1a2d2565afdd
Agent725 <agent725@725.be>
2022-05-19 14:49:48 +0200

    working prototype of Stripe.com gateway

---
+++ data/gateways/stripe.com/forwarder.php
@@ -0,0 +1,50 @@
+<?php
+
+  /*
+   * Stripe's libraries are very picky and get into a fight with FF3 framework.
+   * So we let this simple forwarder load the library and then forward us instead.
+   */
+
+  // set protocol and base site
+  if(isset($_GET['currency']) && isset($_GET['amount']) && isset($_GET['order'])) {
+    if (isset($_SERVER['HTTPS']) &&
+      ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+      isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+      $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+        $SITE = 'https://';
+    } else { $SITE = 'http://'; }
+    $tmp = dirname($_SERVER['REQUEST_URI']);
+    $SITE .= (isset($_SERVER['HTTP_HOST'])?$_SERVER['HTTP_HOST']:'').($tmp=='/'?'/':$tmp.'/');
+
+    $json = json_decode(file_get_contents('gateway.json'));
+    foreach($json as $key => $val) {
+      $GATEWAY[$key] = $val;
+    }
+    $forwardURL = $SITE.'../../..?checkout&x='.$_GET['order'];
+
+    require('lib/vendor/autoload.php');
+    header('Content-Type: application/json');
+    \Stripe\Stripe::setApiKey($GATEWAY['API_Key']);
+    $checkout_session = \Stripe\Checkout\Session::create([
+      'payment_method_types' => ['card', 'ideal', 'bancontact'],  //payment methods
+      'line_items' => [
+        [
+          'price_data' => [
+            'currency' => strtolower($_GET['currency']),    // defines the currency used
+            'unit_amount' => $_GET['amount'],               // defines the price charged
+
+            'product_data' => [
+              'name' => $_GET['order']                      // display name on checkout screen
+            ],
+          ],
+          'quantity' => 1
+        ]
+      ],
+      'mode' => 'payment',
+      'success_url' => $forwardURL.'&s=100',                // redirect on success
+      'cancel_url' => $forwardURL.'&s=99'                   // redirect on cancel
+    ]);
+
+    header("Location: " .$checkout_session->url);
+  }
+?>
--- data/gateways/stripe.com/gateway.php
+++ data/gateways/stripe.com/gateway.php
@@ -2,110 +2,39 @@
 
 /*
  *
- *  GATEWAY FOR PAYING BY MOLLIE.NL
+ *  GATEWAY FOR PAYING WITH STRIPE.COM
  *
  */
 
 $array['payment_method']='stripe.com';
-
 $array['forwardurl'] = $SITE.'?checkout&x='.$array['ordernumber'];
 
 function paymentgate($array,$shop) {
   global $GATEWAY,$SITE,$SET,$STR;
-  // get gateway variables
-  require($GATEWAY['directory'].'settings.php');
-
+  require($GATEWAY['directory'].'settings.php'); // get gateway variables
   if (!isset($array['amount']) || !isset($array['ordernumber'])) {
     echo 'Missing amount or ordernumber!';
     die();
   }
-
-  try {
-    /*
-     * Initialize the Stripe API library with API key.
-     *
-     * See: https://github.com/stripe/stripe-php
-     */
-    // === STRIPE INIT === //
-    require_once($GATEWAY['directory'].'lib/vendor/stripe/stripe-php/init.php');
-
-    $stripe = new \Stripe\StripeClient($GATEWAY['API_Key']);
-    $charge = $stripe->charges->create([
-      'amount' => $shop->formatn($array['amount']),
-      'currency' => strtolower($SET['shopcurrency']),
-      'source' => 'tok_amex', // obtained with Stripe.js
-      'description' => $array['ordernumber']
-    ], [
-      'idempotency_key' => hash('sha256',$array['ordernumber'].$array['amount'].$date->format('Y-m-d'))  // unique ID to avoid charge collisions
-    ]);
-
-    /* DO WE NEED THIS???
-    $customer = $stripe->customers->create([
-        'description' => $array['ordernumber'],
-        'email' => 'email@example.com',
-        'payment_method' => 'pm_card_visa',
-    ]);
-    */
-
-// //
-
-/*
-    $payment = $mollie->payments->create([
-        "amount" => [
-            "currency" => $SET['shopcurrency'],
-            "value" => $shop->formatn($array['amount'])
-        ],
-        "description" => $array['ordernumber'],
-        "redirectUrl" => $array['forwardurl'].'&s=99',
-        "webhookUrl"  => $array['forwardurl'].'&s=100'
-
-    ]);
-
-    $array['forwardurl'] = $payment->getCheckoutUrl();
-*/
-
-    /* DEBUG
-     * If you want to forward the user:
-       // This header location should always be a GET, thus we enforce 303 http response code
-       //header("Location: " . $payment->getCheckoutUrl(), true, 303);
-       * // JUMP WITH THIS FORM DATA: echo '<input type="hidden" name="x" value="'.$shop->tx($array).'" />';
-     * Some debug info:
-    echo '<div style="display: inline-block; text-align: left; background: #FFF none repeat scroll 0% 0%; font-weight: bold; margin: 12px 0; padding: 12px; min-width: 320px; border: 3px dashed #777;"><table>'.
-       '<tr><td style="width: 200px;">API key: </td><td>'.$GATEWAY['API_Key'].'</td></tr>'.
-       '<tr><td style="width: 200px;">Currency: </td><td>'.$SET['shopcurrency'].'</td></tr>'.
-       '<tr><td style="width: 200px;">Amount: </td><td>'.$array['amount'].'</td></tr>'.
-       '<tr><td style="width: 200px;">Ordernumber: </td><td>'.$array['ordernumber'].'</td></tr>'.
-       '<tr><td style="width: 200px;">Payment Url: </td><td>'.$array['forwardurl'].'&s=99</td></tr>'.
-       '<tr><td style="width: 200px;">Webhook Url: </td><td>'.$array['forwardurl'].'&s=100</td></tr>'.
-       '</table></div>'
-    */
-
-
-  } catch(Exception $exc) {
-    $array['forwardurl'] = $strReturnUrl.'?orderId='.$array['ordernumber'].'&orderStatusId=0&error='.$exc->getMessage();
-    return $array;
-  }
-
+  $amountCents = $shop->formatn($array['amount'])*100;
+  $array['forwardurl'] = $SITE.$GATEWAY['directory'].'forwarder.php?currency='.strtolower($SET['shopcurrency']).'&amount='.$amountCents.'&order='.$array['ordernumber'];
   return $array;
-
 }
 
-function paymentform($array,$shop) {
-  global $GATEWAY,$SET,$STR;
-  // get gateway variables
-  require($GATEWAY['directory'].'settings.php');
 
+function paymentform($array,$shop) {
+  global $GATEWAY,$SITE,$SET,$STR;
+  require($GATEWAY['directory'].'settings.php'); // get gateway variables
   if (!isset($array['amount']) || !isset($array['ordernumber'])) {
     echo 'Missing amount or ordernumber!';
     die();
   }
-
   echo '<div style="width: 100%; margin-top: 48px; text-align: center;"><h4>'.$STR['Amount_to_pay'].': <span style="font-weight: bold;">'.$SET['shopcurrency'].' '.$shop->formatn($array['amount']).'</span></h4><br>'.
        '<span>'.$GATEWAY['description'].'</span><br><br>'.
        $STR['Paying_via'].':<br><img class="img-responsive" style="margin: 0 auto;" src="'.$GATEWAY['directory'].'stripe.jpeg" /></div>';
-
   echo "<input type='hidden' name='x' value='".$shop->tx($array)."' />";
-
+  // [!] click submit to forward user to Stripe's own gateway in 3000ms
+  echo "<script>setTimeout( () => { document.querySelector('.submit').click(); }, 3000);</script>";
 }
 
 ?>
