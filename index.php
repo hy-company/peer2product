a9333c31d4219f33514143ab4a49afd24f8b77f9
David <david@dkvdev.com>
2022-09-08 18:53:04 +0200

    css clean up

--- index.php
+++ index.php
@@ -23,10 +23,40 @@
         
       </div>
       <?php include('lib/main.php');?>
-      <div id="footer">Powered by <a href="http://peer2product.com">Peer2Product</a></div>
-    </div>
+      </div>
   </div>
+      <footer>
+      <div class="row">
+        <div class="col-xs-12"><br></div>
+        <div class="col-xs-4">
+        <a href="<?=$SITE;?>terms">
+          <center><i class="fa fa-asterisk fa-2x" aria-hidden="true"></i></center>  
+          <h4><?=$STR['Terms'];?></h4></a>
+          <p>Read about our terms of use, privacy statement and return policy</p>
+        </div>
+
+        <div class="col-xs-4">
+          <a href="<?=$SITE;?>about">
+          <center><i class="fa fa-users fa-2x" aria-hidden="true"></i></center>
+          <h4><?=$STR['About'];?></h4></a>
+          <p>Learn more about our business and the people behind it.</p>
+        </div>
+
+        <div class="col-xs-4">
+          <a href="<?=$SITE;?>contact">
+          <center><i class="fa fa-comments-o fa-2x" aria-hidden="true"></i></center>
+
+          <h4><?=$STR['Contact'];?></h4></a>
+          <p>We'd love to hear from you! Get in touch with us!</p>
+        </div>
+        <div class="col-xs-12"><br><br><br><br></div>
+      </div>
+
+      <div id="footer">Powered by <a href="http://peer2product.com">Peer2Product</a></div>
+   
+  <?php } ?>
+  </footer>
     <?php include('ui/footer.htm'); ?>
   </body>
 </html>
-<?php } ?>
+
