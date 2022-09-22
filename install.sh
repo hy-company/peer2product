e5e15e8c4efe39c467a92a3803ce8240f6e7064e
Agent725 <agent725@725.be>
2022-09-22 16:15:42 +0200

    removed PEAR dependency, now built-in in lib

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 echo "This script installs the dependencies to be able to run Peer2Product."
-sudo apt install php php-pear
+sudo apt install php
 echo " - Configuring Apache2..."
 sudo a2enmod rewrite
 sudo service apache2 restart
-echo " - Installing Pear Mail..."
-sudo pear install Mail
-echo
 echo "All done! Have fun!"
