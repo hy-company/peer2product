fd45a1371c61276ea717ec6311790b17e5e2610b
davidvk89 <davidvankleef@gmail.com>
2023-01-02 13:37:18 +0100

    new footer implementation

--- index.php
+++ index.php
@@ -44,52 +44,6 @@
 <?php include('ui/footer.htm'); ?>
 
 </div>
-<!-- DISABLED DUE TO MERGE, PLEASE FIX =======
-      <div id="shopnav" class="row">
-        <div class="col-md-12" id='shopbanner'>
-          <a href="<?=$SITE;?>"><img class='img-responsive' src="<?php echo ($SET['shopmainlogo']?$SET['data/'].$SET['shopmainlogo']:$DEF['shopmainlogo']); ?>"/></a>
-        </div>
-        <?php include('ui/navbar.htm'); ?>
-
-
-      </div>
-      <?php include('lib/main.php');?>
-      </div>
-  </div>
-      <footer>
-      <div class="row">
-        <div class="col-xs-12"><br></div>
-        <div class="col-xs-4">
-        <a href="<?=$SITE;?>terms">
-          <center><i class="fa fa-asterisk fa-2x" aria-hidden="true"></i></center>  
-          <h4><?=$STR['Terms'];?></h4></a>
-          <p><?=$STR['footer_terms'];?></p>
-        </div>
-
-        <div class="col-xs-4">
-          <a href="<?=$SITE;?>about">
-          <center><i class="fa fa-users fa-2x" aria-hidden="true"></i></center>
-          <h4><?=$STR['About'];?></h4></a>
-          <p><?=$STR['footer_about'];?></p>
-        </div>
-
-        <div class="col-xs-4">
-          <a href="<?=$SITE;?>contact">
-          <center><i class="fa fa-comments-o fa-2x" aria-hidden="true"></i></center>
-
-          <h4><?=$STR['Contact'];?></h4></a>
-          <p><?=$STR['footer_contact'];?></p>
-        </div>
-        <div class="col-xs-12"><br><br><br><br></div>
-        <p id="footer">Powered by <a href="http://peer2product.com">Peer2Product</a></p>
-        <br>
-      </div>
-
-  <?php //} REMOVE ME!!!!
-  ?>
-  </footer>
-    <?php include('ui/footer.htm'); ?>
- beta --!>
-  </body>
+</body>
 </html>
 
