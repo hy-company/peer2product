1661e098b0871046c4414be2457d15bf8c4264e2
davidvk89 <99416738+davidvk89@users.noreply.github.com>
2023-02-24 04:04:32 -0800

    Update endpoint.php
    Additional commenting on how the script works and edits to the example JSON that is being sent to peer2product. Using this format allows us to leverage existing functionalities in peer2product without major reworks.

--- data/themes/hybrix/api/endpoint.php
+++ data/themes/hybrix/api/endpoint.php
@@ -2,15 +2,10 @@
 
 // this is a very early testing build for this script. This script will eventualy allow hybrix to create
 // orders on the peer2product platform for vouchers.
-//currently accepts a post request to execute a function that places a custom order into the orders folder.
-//current itteration places the script and allows it to be seen and used from the admin dashboard.
-//It currently does not yet show a name, last name or adress etc. just yet. This is the next todo.
-
-$config_json = file_get_contents("config.json");
-$config = json_decode($config_json);
 
 function create_order_deposit($payload){
     $order_id = uniqid();
+    $array = (array) $payload;
     $order = '{"'.$order_id . '" :' . $payload .  '}';
     $order_file = fopen("../../../orders/$order_id.json", "w");
     fwrite($order_file, $order);
@@ -19,18 +14,18 @@ function create_order_deposit($payload){
 }
 
 function create_order_withdraw($payload){
-//create a withdraw order   
+    //this function seems redundant as users can simply use the gateway to buy hy euro.  
 }
 
 function create_order_transfer($payload){
-    // create a transfer order.
+    //this function seems redundant as users can use the deposit function with the transfer target's adress.
 }
 
-if(isset($_POST['hybrix_request'])){
-//recieved data is in JSON format:
+if(isset($_POST['hybrix_request'])){ //
+    
     $request = json_decode($_POST['payload']);
 
-    switch(request->type){
+    switch($request->type){
         case "deposit";
             create_order_deposit($request);
             break;
@@ -49,15 +44,33 @@ if(isset($_POST['hybrix_request'])){
     }
 }
 
+
+// Below you'll find an example payload to send to peer2product. Using this format allows us to use the system
+// as is instead of having to reprogram large parts of it in order to behave nicely. 
+// Sending in a POST request with $_POST['hybrix_request] : true allows this script to execute.
+// attach the JSON example format below to $_POST['payload'] to create an order. 
+
+$time = time(); 
 $payload =  // IMPORTANT: [] in the target adress BREAKS THE ADMIN ->orders foreach loop. (lib/functions.php:616)
+            // IMPORTANT: TIME MUST BE UNIX TIMESTAMP.
+            // USERS TARGET ADRESS IS SET UNDER STREETNAME. THIS ALLOWS FOR AN EASIER VIEWING OF THE ORDER.
+            // USERS TARGET BANK ACCOUNT SHOULD BE PLACED UNDER REMARKS.
 '
 {
+    "firstname" : "ENTER USER INITIALS/FIRSTNAME AS ON BANK ACCOUNT",
+    "lastname" : "ENTER USER LASTNAME AS ON BANK ACCOUNT",
+    "company" : "hybrix",
+    "time" : '. $time . ', 
+    "notransport": 0,
+    "transport": 0,
     "type" : "deposit",
     "amount" : 10,
     "margin" : 2.5,
     "tariff" : 0,
-    "e-mail" : "agent725@725.be",
-    "target" : "0xa12b3c4d5e..."    
+    "e-mail" : "ENTER USER EMAIL ADRESS",
+    "streetname" : "0xa12b3c4d5e...",
+    "remarks"     : "USERS TARGET ADRESS FOR A DEPOSIT,WITHDRAWAL OR TRANSFER.",
+    "product-table" : "<div>ORDER TYPE: {deposit}<br> TO: {target adress}<br> AMOUNT: {amount}<br> NAME: {BANK ACC OWNER NAME}</div>"
   }
 ';
 create_order_deposit($payload);
