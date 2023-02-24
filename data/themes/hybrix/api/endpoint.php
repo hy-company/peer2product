3e94f39f9d1aa25ef88dcd58e2baee5ea0ce0788
davidvk89 <99416738+davidvk89@users.noreply.github.com>
2023-02-24 05:31:16 -0800

    OO method for the hybrix p2p link.
    Added a third (OO) method on how to handle incoming requests from hybrix.

--- data/themes/hybrix/api/endpoint.php
+++ data/themes/hybrix/api/endpoint.php
@@ -6,15 +6,17 @@
 $config_json = file_get_contents("config.json");
 $config = json_decode($config_json);
 
+//METHOD 1: Recieve a ready to use JSON payload that's ready to be inserted as an order.
 function create_order_deposit($payload){
     $order_id = uniqid();
     $order = '{"'.$order_id . '" :' . $payload .  '}';
     $order_file = fopen("../../../orders/$order_id.json", "w");
     fwrite($order_file, $order);
-    
+    echo "method 1:<br>";
     echo $order_id; //return the order id to hybrix
 }
 
+//METHOD 2: Recieve the nessecary data and merge it with additional data required to make the order work.
 function create_order_deposit_method_2($payload){
     $order_id = uniqid();
     $peer2product_data = [];
@@ -40,6 +42,20 @@ function create_order_deposit_method_2($payload){
     echo $order_id; //return the order id to hybrix
 }
 
+//METHOD 3: Recieve the minimum required data, build a new object with this data that can be interpreted by p2p.
+require("obj/flex_voucher.php");
+function create_order_deposit_method_3($payload){
+   $voucher = new OrderDetails($payload);
+   $order_json = json_encode($voucher);
+   $order = '{"'.$voucher->order_id . '" :' . $order_json .  '}';
+   $order_file = fopen("../../../orders/$voucher->order_id.json", "w");
+   fwrite($order_file, $order);
+   print_r($voucher);
+   echo "<br>Method 3: <br>";
+   echo $voucher->order_id;
+    
+    
+}
 function create_order_withdraw($payload){
     //this function seems redundant as users can simply use the gateway to buy hy euro.  
 }
@@ -72,17 +88,20 @@ if(isset($_POST['hybrix_request'])){ //
 }
 
 
-// Below you'll find an example payload to send to peer2product. Using this format allows us to use the system
+// Below you'll find an example payloads to send to peer2product. Using this format allows us to use the system
 // as is instead of having to reprogram large parts of it in order to behave nicely. 
 // Sending in a POST request with $_POST['hybrix_request] : true allows this script to execute.
 // attach the JSON example format below to $_POST['payload'] to create an order. 
 
+// IMPORTANT: [] in the target adress BREAKS THE ADMIN ->orders foreach loop. (lib/functions.php:616)
+// IMPORTANT: DO NOT USE - (DASH) IN LABELS. THIS CAUSES ISSUES WITH PHP VARIABLES. INSTEAD USE _ (UNDERSCORE)
+// IMPORTANT: TIME MUST BE UNIX TIMESTAMP.
+// USERS TARGET ADRESS IS SET UNDER STREETNAME. THIS ALLOWS FOR AN EASIER VIEWING OF THE ORDER.
+// USERS TARGET BANK ACCOUNT SHOULD BE PLACED UNDER REMARKS.
+
+//EXAMPLE 1 
 $time = time(); 
-$payload =  // IMPORTANT: [] in the target adress BREAKS THE ADMIN ->orders foreach loop. (lib/functions.php:616)
-            // IMPORTANT: TIME MUST BE UNIX TIMESTAMP.
-            // USERS TARGET ADRESS IS SET UNDER STREETNAME. THIS ALLOWS FOR AN EASIER VIEWING OF THE ORDER.
-            // USERS TARGET BANK ACCOUNT SHOULD BE PLACED UNDER REMARKS.
-'
+$payload =  '
 {
     "firstname" : "ENTER USER INITIALS/FIRSTNAME AS ON BANK ACCOUNT",
     "lastname" : "ENTER USER LASTNAME AS ON BANK ACCOUNT",
@@ -100,9 +119,13 @@ $payload =  // IMPORTANT: [] in the target adress BREAKS THE ADMIN ->orders fore
     "product-table" : "<div>ORDER TYPE: {deposit}<br> TO: {target adress}<br> AMOUNT: {amount}<br> NAME: {BANK ACC OWNER NAME}</div>"
   }
 ';
+
+$method_1_start = microtime(true);
 create_order_deposit($payload);
+$method_1_end = microtime(true);
+$method_1_execution_time = $method_1_end - $method_1_start;
 
-
+// EXAMPLE 2
 $payload = "";
 $payload = '
 {
@@ -111,13 +134,32 @@ $payload = '
     "margin" : 2.5,
     "tariff" : 0,
     "e-mail" : "agent725@725.be",
-    "target" : ["0xa12b3c4d5e..."]
+    "target" : "0xa12b3c4d5e..."
   }
 ';
 
-
+$method_2_start = microtime(true);
 create_order_deposit_method_2($payload);
+$method_2_end = microtime(true);
+$method_2_execution_time = $method_2_end - $method_2_start;
 
+// EXAMPLE 3
+$payload = "";
+$payload = '
+{
+    "firstname" : "TEST",
+    "lastname"  : "NAME",
+    "type" : "deposit",
+    "amount" : 10,
+    "margin" : 2.5,
+    "tariff" : 0,
+    "e-mail" : "agent725@725.be",
+    "target" : "0xa12b3c4d5e..."
+  }
+';
+ create_order_deposit_method_3($payload);
 
+echo "<br>Method 1 execution time: $method_1_execution_time";
+echo "<br>Method 1 execution time: $method_2_execution_time";
 ?>
 
